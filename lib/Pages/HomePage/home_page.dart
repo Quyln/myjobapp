65d9a2bc5eac27e60c503937317b4fed1dc02420
Quyln <kayq9999@gmail.com>
2023-11-23 16:17:00 +0700

    update

--- lib/Pages/HomePage/home_page.dart
+++ lib/Pages/HomePage/home_page.dart
@@ -2,52 +2,69 @@ import 'package:flutter/material.dart';
 import 'package:myjobapp/Pages/HomePage/home_tinnoibat_section.dart';
 import 'package:myjobapp/Pages/HomePage/home_top_sec.dart';
 import 'package:myjobapp/Pages/HomePage/home_viecmoinhat_section.dart';
+import 'package:myjobapp/Provider/login_getuser_provider.dart';
 import 'package:myjobapp/utils/colors_texts_style.dart';
+import 'package:provider/provider.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({
     super.key,
   });
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: CustomScrollView(
-        slivers: [
-          const HSecAppBar(),
-          SliverList(
-              delegate: SliverChildListDelegate([
-            const SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Padding(
-                    padding: EdgeInsets.only(left: 20, bottom: 10),
-                    child: Text(
-                      'Tin nổi bật',
-                      style: tHeadline,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<GetUserProvider>(
+          create: (context) {
+            return GetUserProvider();
+          },
+        ),
+      ],
+      child: Scaffold(
+        backgroundColor: Colors.white,
+        body: CustomScrollView(
+          slivers: [
+            const HSecAppBar(),
+            SliverList(
+                delegate: SliverChildListDelegate([
+              const SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: 20,
                     ),
-                  ),
-                  TinNoiBatHSec(),
-                  Padding(
-                    padding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
-                    child: Text(
-                      'Việc làm mới nhất',
-                      style: tHeadline,
+                    Padding(
+                      padding: EdgeInsets.only(left: 20, bottom: 10),
+                      child: Text(
+                        'Tin nổi bật',
+                        style: tHeadline,
+                      ),
                     ),
-                  ),
-                  ViecmoinhatHSec(),
-                  SizedBox(
-                    height: 100,
-                  )
-                ],
+                    TinNoiBatHSec(),
+                    Padding(
+                      padding: EdgeInsets.only(left: 20, top: 10, bottom: 10),
+                      child: Text(
+                        'Việc làm mới nhất',
+                        style: tHeadline,
+                      ),
+                    ),
+                    ViecmoinhatHSec(),
+                    SizedBox(
+                      height: 100,
+                    )
+                  ],
+                ),
               ),
-            ),
-          ]))
-        ],
+            ]))
+          ],
+        ),
       ),
     );
   }
--- lib/Pages/HomePage/home_top_sec.dart
+++ lib/Pages/HomePage/home_top_sec.dart
@@ -32,98 +32,93 @@ class _HSecAppBarState extends State<HSecAppBar> {
   );
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider<GetUserProvider>(
-      create: (context) {
-        return GetUserProvider();
-      },
-      child: Consumer<GetUserProvider>(
-        builder: (context, value, child) {
-          final token = NumberFormat('#,###').format(value.user.token);
-          return SliverAppBar(
-            floating: true,
-            pinned: false,
-            backgroundColor: Colors.white,
-            leadingWidth: 290,
-            leading: Row(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.only(left: 15),
-                  child: InkWell(
-                    onTap: () {},
-                    child: CircleAvatar(
-                      radius: 20,
-                      backgroundColor: Colors.white,
-                      backgroundImage: NetworkImage(value.user.avatar),
-                    ),
+    return Consumer<GetUserProvider>(
+      builder: (context, value, child) {
+        final token = NumberFormat('#,###').format(value.user.token);
+        return SliverAppBar(
+          floating: true,
+          pinned: false,
+          backgroundColor: Colors.white,
+          leadingWidth: 290,
+          leading: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 15),
+                child: InkWell(
+                  onTap: () {},
+                  child: CircleAvatar(
+                    radius: 20,
+                    backgroundColor: Colors.white,
+                    backgroundImage: NetworkImage(value.user.avatar),
                   ),
                 ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SizedBox(
-                      width: 210,
-                      child: Text(
-                        value.user.position == 'Thành viên'
-                            ? value.user.fullname
-                            : value.user.companyname,
-                        style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                            color: Colors.black),
-                        overflow: TextOverflow.ellipsis,
-                      ),
-                    ),
-                    Text(
-                      value.user.position,
+              ),
+              const SizedBox(
+                width: 10,
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 210,
+                    child: Text(
+                      value.user.position == 'Thành viên'
+                          ? value.user.fullname
+                          : value.user.companyname,
                       style: const TextStyle(
-                          fontStyle: FontStyle.normal,
-                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
                           color: Colors.black),
+                      overflow: TextOverflow.ellipsis,
                     ),
-                  ],
-                ),
-              ],
-            ),
-            actions: [
-              Padding(
-                padding: const EdgeInsets.only(right: 15),
-                child: Row(
-                  children: [
-                    Text(
-                      token,
-                      style: const TextStyle(
-                          fontWeight: FontWeight.normal,
-                          color: Colors.red,
-                          fontSize: 16),
-                    ),
-                    InkWell(
-                      onTap: () async {
-                        SharedPreferences pref =
-                            await SharedPreferences.getInstance();
-                        await pref.setBool('checklogin', false);
-
-                        Navigator.pushReplacement(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => const LoginScreen()));
-                      },
-                      child: Image.network(
-                        'https://th.bing.com/th/id/R.cef7ade7807f8c0d60886922e91316c2?rik=vM3X8FUbkNUInw&pid=ImgRaw&r=0',
-                        width: 30,
-                        height: 30,
-                      ),
-                    )
-                  ],
-                ),
-              )
+                  ),
+                  Text(
+                    value.user.position,
+                    style: const TextStyle(
+                        fontStyle: FontStyle.normal,
+                        fontSize: 14,
+                        color: Colors.black),
+                  ),
+                ],
+              ),
             ],
-          );
-        },
-      ),
+          ),
+          actions: [
+            Padding(
+              padding: const EdgeInsets.only(right: 15),
+              child: Row(
+                children: [
+                  Text(
+                    token,
+                    style: const TextStyle(
+                        fontWeight: FontWeight.normal,
+                        color: Colors.red,
+                        fontSize: 16),
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      SharedPreferences pref =
+                          await SharedPreferences.getInstance();
+                      await pref.setBool('checklogin', false);
+
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const LoginScreen()));
+                    },
+                    child: Image.network(
+                      'https://th.bing.com/th/id/R.cef7ade7807f8c0d60886922e91316c2?rik=vM3X8FUbkNUInw&pid=ImgRaw&r=0',
+                      width: 30,
+                      height: 30,
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        );
+      },
     );
   }
 }
